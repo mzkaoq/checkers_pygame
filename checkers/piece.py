07791495ff8f8d42eca265cc0515e7e152d6c304
mzkaoq <mzkaoq333@gmail.com>
2021-06-04 23:50:07 +0200

    dodanie rysowania króla,promocja na króla, wstępne przemieszczanie bierek

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,5 +1,5 @@
 import pygame
-from checkers.constants import GREY, WHITE, SQUARE_SIZE
+from checkers.constants import GREY, WHITE, SQUARE_SIZE, CROWN
 
 
 class Piece:
@@ -30,6 +30,13 @@ class Piece:
         radius = SQUARE_SIZE//2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x,self.y),radius+self.BORDER)
         pygame.draw.circle(win, self.color, (self.x,self.y),radius)
+        if self.king:
+            win.blit(CROWN, (self.x-CROWN.get_width()//2,self.y-CROWN.get_height()//2))
 
     def __repr__(self):
         return str(self.color)
+
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
 from checkers.board import Board
 
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("checkers")
+
+def get_row_col_from_mouse(pos):
+    x,y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row,col
 
 def main():
     run = True
@@ -18,7 +24,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row,col)
+                board.move(piece, 7,3)
+
         board.draw(WIN)
         pygame.display.update()
     pygame.quit()
